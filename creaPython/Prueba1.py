573279a1f0fc4dac19bc5b87479a8989b1242a34
Julian David Fierro Casanova <julifierrocasanova@gmail.com>
2024-05-28 08:50:19 -0500

    python

---
+++ creaPython/Prueba1.py
@@ -0,0 +1,93 @@
+
+print("") 
+print("") 
+
+edad=5
+print("Tu edad es:")
+print(edad)
+
+
+print("") 
+print("") 
+a = 10
+b = 3
+
+print("OPERACIONES:")
+Suma = a+b
+Resta = a-b
+Multiplicar = a*b
+Dividir = a/b
+DividirEntero = a//b
+Modulo = a%b
+print("La suma de", a, "+", b, "=", Suma)
+print("La Resta de", a, "-", b, "=", Resta)
+print("La Multiplicar de", a, "*", b, "=", Multiplicar)
+print("La Dividir de", a, "/", b, "=", Dividir)
+print("La DividirEntero de", a, "//", b, "=", DividirEntero)
+print("La Modulo de", a, "%", b, "=", Modulo)
+
+
+print("") 
+print("") 
+contador = 1
+contador += 1 
+print("La Suma del contador es:",contador) 
+
+
+a = 10
+a -= 5 
+print("La Resta del contador es:",a) 
+b = 3
+b *= 2 
+print("La Multiplicar del contador es:",b) 
+c = 10
+c /= 2 
+print("La Dividir del contador es:",c) 
+
+
+
+
+print("") 
+print("") 
+
+print("Igualdad: se utiliza el operador == Por ejemplo:")
+a = 5
+b = 5
+resultado = a == b
+print(resultado) 
+# Imprime True
+
+print("") 
+print("Desigualdad: se utiliza el operador != Por ejemplo: ")
+
+a = 5
+b = 3
+resultado = a != b
+print(resultado) 
+
+print("") 
+# # Imprime True
+print("Mayor que: se utiliza el operador > Por ejemplo:")
+a = 5
+b = 3
+resultado = a > b
+print(resultado) 
+# Imprime True
+
+print("") 
+print("Menor que: se utiliza el operador < Por ejemplo:")
+a = 5
+b = 3
+resultado = a < b
+print(resultado) 
+# Imprime False
+
+print("Mayor o igual que y menor o igual que: se utilizan los operadores >= y <= Por ejemplo:")
+a = 5
+b = 5
+resultado = a >= b
+print(resultado) # Imprime True
+c = 3
+d = 5
+resultado = c <= d
+print(resultado) # Imprime True
