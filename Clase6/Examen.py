3f42fd4d4e963bc5756c85c7f1066410d1010069
Julian David Fierro Casanova <julifierrocasanova@gmail.com>
2024-06-06 10:15:08 -0500

    Subo cambio modal

---
+++ Clase6/Examen.py
@@ -0,0 +1,33 @@
+# a = "manzana"
+# b = "platano"
+
+# x,y=b,a 
+# print(x,y,sep="::")
+
+# a = "manzana"
+# b = "platano"
+# x, y = b, a
+# print(x, y, sep="::")
+
+
+# nums = [1, 2, 3, 4, 5, 6, 7]
+# print(nums[::-1])
+
+# if "cat" == "dog": 
+#     print("prrrr") 
+# else: print("ruff")
+
+
+# frase = "hola, la vida es bella"
+# frase_capitalizada = frase.capitalize()
+# print(frase_capitalizada)
+
+
+    
+
+# def fun(x):
+#     return ["Tea"]  
+
+# coffees = ["Cappuccino", "Latte", "Macchiato"] 
+# tea = fun(coffees)
+# print(tea  
